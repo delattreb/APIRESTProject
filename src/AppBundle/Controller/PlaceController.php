bca209427c9239ff18cf008b6f9b0ccb9140e849
Bruno DELATTRE <delattreb@gmail.com>
2018-02-10 12:11:08 +0100

    Add 404 error gesture

--- src/AppBundle/Controller/PlaceController.php
+++ src/AppBundle/Controller/PlaceController.php
@@ -7,10 +7,34 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Place;
 
 class PlaceController extends Controller
 {
+    /**
+     * @Route("/places/{place_id}", name="places_one")
+     * @Method({"GET"})
+     */
+    public function getPlaceAction(Request $request)
+    {
+        $place = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:Place')
+            ->find($request->get('place_id'));
+        /* @var $place Place */
+
+        if (empty($place)) {
+            return new JsonResponse(['message' => 'Place not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $formatted = [
+            'id' => $place->getId(),
+            'name' => $place->getName(),
+            'address' => $place->getAddress(),
+        ];
+        return new JsonResponse($formatted);
+    }
+
     /**
      * @Route("/places", name="places_list")
      * @Method({"GET"})
--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -7,11 +7,35 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\User;
 
 class UserController extends Controller
 {
+    /**
+     * @Route("/users/{id}", name="users_one")
+     * @Method({"GET"})
+     */
+    public function getUserAction(Request $request)
+    {
+        $user = $this->get('doctrine.orm.entity_manager')
+            ->getRepository('AppBundle:User')
+            ->find($request->get('id'));
+        /* @var $user User */
+
+        if (empty($user)) {
+            return new JsonResponse(['message' => 'User not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $formatted = [
+            'id' => $user->getId(),
+            'firstname' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+            'email' => $user->getEmail(),
+        ];
+        return new JsonResponse($formatted);
+    }
+
     /**
      * @Route("/users", name="users_list")
      * @Method({"GET"})
