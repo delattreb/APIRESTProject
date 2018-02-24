21abbeff7bb661927e4442895e75e1caf7995c1f
Bruno DELATTRE <delattreb@gmail.com>
2018-02-24 06:28:55 +0100

    Remove Migration & Fixtures

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,7 +19,6 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new AppBundle\AppBundle(),
         ];
 
